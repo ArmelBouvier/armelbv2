ed6c7923a9e0a6844c3ec026766503a74b95d992
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2021-02-28 11:51:45 +0100

    portfolio basis ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import NavItem from "./NavItem";
+
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { NavItemActive: "" };
+    this.state = {
+      NavItemActive: "",
+    };
   }
   activeitem = (x) => {
     if (this.state.NavItemActive.length > 0) {
-      document.getElementById(this.state.NavItemActive).classList.remove("active");
+      document
+        .getElementById(this.state.NavItemActive)
+        .classList.remove("active");
     }
-    this.setState({ NavItemId: x }, () => {
+    this.setState({ NavItemActive: x }, () => {
       document.getElementById(this.state.NavItemActive).classList.add("active");
     });
   };
@@ -43,4 +48,5 @@ class Navbar extends Component {
     );
   }
 }
+
 export default Navbar;
